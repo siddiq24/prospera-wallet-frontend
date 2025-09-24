3055314e102a90fe2674de6fe3a4e30f58500000
siddiq24 <siddiqrm24@gmail.com>
2025-09-24 20:05:53 +0700

    fix(main): rename router path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './router'
+import App from './Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
