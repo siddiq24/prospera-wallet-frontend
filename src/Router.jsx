f426457728d943f2dfbf439d9e05d7c95e34299c
Cristian Yulma <cristianyulmaw@gmail.com>
2025-09-25 00:29:51 +0700

    fix:  adding route for dashboard

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Rules } from "./pages/Rules"
+import Dashboard  from "./pages/Dashboard.jsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/rules" element={<Rules />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </BrowserRouter>
     )
