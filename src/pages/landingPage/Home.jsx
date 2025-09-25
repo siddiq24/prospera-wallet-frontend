ed4579a41085bb2b4743f50342451455cb4cf2db
siddiq24 <siddiqrm24@gmail.com>
2025-09-25 16:33:32 +0700

    feat(Home): add component footer to home page

--- src/pages/landingPage/Home.jsx
+++ src/pages/landingPage/Home.jsx
@@ -1,5 +1,6 @@
+import { Link } from "lucide-react";
 import React from "react";
-import { Link } from "react-router";
+import Footer from "../../components/Footer";
 
 const BENEFITS = [
   {
@@ -71,7 +72,7 @@ const sectionPadding = "px-8 md:px-28";
 
 export const Home = () => {
   return (
-    <main className="flex flex-col gap-18">
+    <main className="flex flex-col gap-18 overflow-hidden">
       {/* Hero section */}
       <div
         className={`bg-[#2948FF]  text-white flex flex-col pt-20 gap-6  md:items-center ${sectionPadding}`}
@@ -87,22 +88,23 @@ export const Home = () => {
             </div>
             <div className="flex gap-8">
               {DOWNLOAD_BUTTONS.map((el, idx) => (
-                <Link
+                <div
                   key={idx}
-                  to={""}
                   style={{
                     backgroundColor: el.bg,
                     color: el.text,
                     borderColor: el.border,
                     borderWidth: "1px",
                   }}
-                  className="w-full px-6 py-4 flex items-center justify-center gap-4 rounded-md"
-                >
-                  <span>
-                    <img src={el.img} alt={el.title} />
-                  </span>
-                  <span>{el.title}</span>
-                </Link>
+                  className="w-full px-6 py-4 flex items-center justify-center gap-4 rounded-md">
+                  <Link to={""}
+                  >
+                    <span>
+                      <img src={el.img} alt={el.title} />
+                    </span>
+                    <span>{el.title}</span>
+                  </Link>
+                </div>
               ))}
             </div>
             <div className="flex gap-4 items-center">
@@ -255,7 +257,7 @@ export const Home = () => {
           </div>
 
           {/* Blue section */}
-          <div className="rounded-2xl bg-[#2948FF] w-full flex flex-col items-center px-4 py-8 gap-2">
+          <div className="rounded-2xl bg-[#2948FF] w-[50%]  flex flex-col items-center px-4 py-8 gap-2">
             {/* User Review Photo */}
             <div>
               <img src="james-bond.png" alt="James Bond" />
@@ -292,53 +294,49 @@ export const Home = () => {
       </div>
 
       {/* Download The App */}
-      <div className={`flex flex-col gap-4 ${sectionPadding}`}>
-        <div className="font-medium text-4xl text-[#0B132A]">
-          Download The App
-        </div>
-        <div className="font-normal text-lg text-[#0B132A]">
-          Ready to experience the future of payments? Download e-wallet now and
-          enjoy a world of convenience at your fingertips.
-        </div>
+      <div className={`flex flex-col md:flex-row-reverse gap-4 ${sectionPadding}`}>
+        <section className=" flex-1 flex flex-col justify-center gap-6 p-33 pl-0">
+          <div className="font-medium text-4xl text-[#0B132A]">
+            Download The App
+          </div>
+          <div className="font-normal text-lg text-[#0B132A]">
+            Ready to experience the future of payments? Download e-wallet now and
+            enjoy a world of convenience at your fingertips.
+          </div>
 
-        {/* Download Button */}
-        <div className="flex gap-8">
-          <Link
-            to={""}
-            className="bg-[#2948FF] text-[#ffffff] w-full px-6 py-4 flex items-center justify-center gap-4 rounded-md"
-          >
-            <span>
-              <img src="play-store.svg" alt="Play Store" />
-            </span>
-            <span>Play Store</span>
-          </Link>
-          <Link
-            to={""}
-            className="bg-white text-[#2948FF] border border-[#2948FF] w-full px-6 py-4 flex items-center justify-center gap-4 rounded-md"
-          >
-            <span>
-              {/* Inline SVG for blue color */}
-              <svg
-                width="24"
-                height="24"
-                fill="#2948FF"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11.5787 9.29677C11.5609 7.17243 13.3173 6.13993 13.3944 6.09246C12.3975 4.64458 10.8547 4.44283 10.3147 4.42503C9.02114 4.28855 7.76315 5.20237 7.10449 5.20237C6.43395 5.20237 5.41332 4.44283 4.32148 4.46063C2.91514 4.48437 1.59188 5.29731 0.873876 6.56717C-0.615536 9.14842 0.494106 12.9461 1.92418 15.0349C2.64218 16.0555 3.47293 17.2007 4.5707 17.1592C5.63881 17.1177 6.04232 16.4768 7.32998 16.4768C8.6117 16.4768 8.98554 17.1592 10.1011 17.1355C11.2523 17.1177 11.9703 16.1089 12.6646 15.0764C13.4894 13.9074 13.8217 12.7503 13.8335 12.691C13.8039 12.6791 11.6024 11.8365 11.5787 9.29677Z"
-                  fill="#2948FF"
-                />
-                <path
-                  d="M9.47225 3.05435C10.0478 2.33635 10.4395 1.35725 10.3327 0.360352C9.50192 0.395955 8.45755 0.935941 7.85822 1.64208C7.33011 2.26514 6.85539 3.27984 6.97407 4.2352C7.91163 4.30641 8.87292 3.76048 9.47225 3.05435Z"
-                  fill="#2948FF"
-                />
-              </svg>
-            </span>
-            <span>Apps Store</span>
-          </Link>
-        </div>
+          {/* Download Button */}
+          <div className="flex gap-8 h-14 w-full">
+            <div className="bg-[#2948FF] flex items-center justify-center flex-1 h-full text-[#ffffff] px-6 py-4 gap-4 rounded-md">
+              <img src="play-store.svg" alt="Play Store" /> Play Store
+            </div>
 
-        <div className="flex justify-center">
+
+            <div className="bg-white h-full text-[#2948FF] border border-[#2948FF] px-6 py-4 flex flex-1 items-center justify-center gap-4 rounded-md">
+              <div >
+                {/* Inline SVG for blue color */}
+                <svg
+                  width="24"
+                  height="24"
+                  fill="#2948FF"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.5787 9.29677C11.5609 7.17243 13.3173 6.13993 13.3944 6.09246C12.3975 4.64458 10.8547 4.44283 10.3147 4.42503C9.02114 4.28855 7.76315 5.20237 7.10449 5.20237C6.43395 5.20237 5.41332 4.44283 4.32148 4.46063C2.91514 4.48437 1.59188 5.29731 0.873876 6.56717C-0.615536 9.14842 0.494106 12.9461 1.92418 15.0349C2.64218 16.0555 3.47293 17.2007 4.5707 17.1592C5.63881 17.1177 6.04232 16.4768 7.32998 16.4768C8.6117 16.4768 8.98554 17.1592 10.1011 17.1355C11.2523 17.1177 11.9703 16.1089 12.6646 15.0764C13.4894 13.9074 13.8217 12.7503 13.8335 12.691C13.8039 12.6791 11.6024 11.8365 11.5787 9.29677Z"
+                    fill="#2948FF"
+                  />
+                  <path
+                    d="M9.47225 3.05435C10.0478 2.33635 10.4395 1.35725 10.3327 0.360352C9.50192 0.395955 8.45755 0.935941 7.85822 1.64208C7.33011 2.26514 6.85539 3.27984 6.97407 4.2352C7.91163 4.30641 8.87292 3.76048 9.47225 3.05435Z"
+                    fill="#2948FF"
+                  />
+                </svg>
+              </div>
+              <span>Apps Store</span>
+            </div>
+
+          </div>
+        </section>
+
+        <div className="flex justify-center flex-1">
           <img className="-mb-6" src="mobile-dashboard-v2.png" alt="" />
           <img
             width="250px"
@@ -348,6 +346,7 @@ export const Home = () => {
           />
         </div>
       </div>
+      <Footer />
     </main>
   );
 };
