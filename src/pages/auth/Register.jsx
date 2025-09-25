bfdf2c116f4714053140cca5a0824127e8a5eaeb
unknown <malailiyati107@gmail.com>
2025-09-25 09:37:47 +0700

    chore: move images from src/assets to public

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
 import "/src/assets/styles/index.css";
-import dompetkecil from "../../assets/dompetkecil.png";
-import google from "../../assets/google.png";
-import fb from "../../assets/fb.png";
-import register from "../../assets/register.png";
-import email from "../../assets/email.png";
-import pwd from "../../assets/password.png";
-// import eyesolid from "";
-// import eyeslash from "";
 import { Link } from "react-router-dom";
 import { Eye, EyeOff } from "lucide-react";
 
@@ -16,8 +8,8 @@ function Register() {
   const [showConfPwd, setShowConfPwd] = useState(false);
   const [errors, setErrors] = useState({});
   const [message, setMessage] = useState("");
-  const eyesolid = <Eye/>
-  const eyeslash = <EyeOff/>
+  const eyesolid = <Eye />;
+  const eyeslash = <EyeOff />;
 
   const [form, setForm] = useState({
     email: "",
@@ -83,7 +75,7 @@ function Register() {
       <section className="flex min-h-screen bg-white md:bg-[var(--color--primary)]">
         <div className="w-full md:w-1/2 rounded-r-4xl bg-white flex flex-col justify-center px-20 py-20">
           <div className="flex items-center text-[var(--color--primary)]">
-            <img src={dompetkecil} alt="dompet" className="w-8 h-8" />
+            <img src="/dompetkecil.png" alt="dompet" className="w-8 h-8" />
             <p>E-Wallet</p>
           </div>
           <h1 className="font-medium text-3xl">
@@ -98,11 +90,11 @@ function Register() {
 
           <div className="mt-6 space-y-3">
             <div className="flex justify-center items-center gap-3 border border-gray-300 w-full rounded-full py-2">
-              <img src={google} alt="google logo" />
+              <img src="/google.png" alt="google logo" />
               <span>Sign In With Google</span>
             </div>
             <div className="flex justify-center items-center gap-3 border border-gray-300 w-full rounded-full py-2">
-              <img src={fb} alt="facebook logo" />
+              <img src="/fb.png" alt="facebook logo" />
               <span>Sign In With Facebook</span>
             </div>
           </div>
@@ -127,7 +119,7 @@ function Register() {
                   className="border border-gray-300 bg-[#FCFDFE] rounded-lg py-2 px-10 my-2 w-full"
                 />
                 <img
-                  src={email}
+                  src="/email.png"
                   alt="logo email"
                   className="absolute w-4 h-4 left-4 top-5"
                 />
@@ -150,14 +142,16 @@ function Register() {
                   className="border border-gray-300 bg-[#FCFDFE] rounded-lg py-2 px-10 my-2 w-full"
                 />
                 <img
-                  src={pwd}
+                  src="/password.png"
                   alt="password logo"
                   className="absolute w-5 h-5 left-4 top-4.5"
                 />
                 <div
                   className="absolute w-5 h-5 right-4 top-4.5 cursor-pointer"
                   onClick={() => setShowPwd(!showPwd)}
-                >{showPwd ? eyesolid : eyeslash}</div>
+                >
+                  {showPwd ? eyesolid : eyeslash}
+                </div>
               </div>
               {errors.pwd && (
                 <p className="text-red-500 text-sm">{errors.pwd}</p>
@@ -176,19 +170,21 @@ function Register() {
                   className="border border-gray-300 bg-[#FCFDFE] rounded-lg py-2 px-10 my-2 w-full"
                 />
                 <img
-                  src={pwd}
+                  src="/password.png"
                   alt="password logo"
                   className="absolute w-5 h-5 left-4 top-4.5"
                 />
                 <img
-                  src={pwd}
+                  src="/password.png"
                   alt="password logo"
                   className="absolute w-5 h-5 left-4 top-4.5"
                 />
                 <div
                   className="absolute w-5 h-5 right-4 top-4.5 cursor-pointer"
                   onClick={() => setShowConfPwd(!showConfPwd)}
-                >{showConfPwd ? eyesolid : eyeslash}</div>
+                >
+                  {showConfPwd ? eyesolid : eyeslash}
+                </div>
               </div>
               {errors.confpwd && (
                 <p className="text-red-500 text-sm">{errors.confpwd}</p>
@@ -208,7 +204,11 @@ function Register() {
         </div>
 
         <div className="hidden md:flex md:w-1/2 justify-center items-center">
-          <img src={register} alt="logo register" className="w-150 h-135" />
+          <img
+            src="/register.png"
+            alt="logo register"
+            className="w-150 h-135"
+          />
         </div>
       </section>
     </>
