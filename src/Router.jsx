90323297c1b09dbbe649627e0fcf79794dd515cc
radifan9 <radif.aghnadiin@gmail.com>
2025-09-25 13:42:40 +0700

    feat: add Home page and its router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,16 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Rules } from "./pages/Rules"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Rules } from "./pages/Rules";
+import { Home } from "./pages/landingPage/Home";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/rules" element={<Rules />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-export default App
+export default App;
