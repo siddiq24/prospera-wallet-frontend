06c60156eb443cd4e65df2713b4bfef570fd6a6b
unknown <malailiyati107@gmail.com>
2025-09-26 10:23:30 +0700

    merge update from origin/dev

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,8 +6,7 @@ import { Home } from "./pages/landingPage/Home";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import Dashboard from "./pages/dashboard/Dashboard";
 import EditProfile from "./pages/profile/EditProfile";
-import Footer from "./components/Footer";
-import { LoggedNavbar } from "./components/Navbar";
+import { LoggedNavbar, Navbar } from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import ChangePassword from "./pages/profile/ChangePassword";
 import ChangePin from "./pages/profile/ChangePin";
@@ -20,11 +19,18 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/rules" element={<Rules />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/pin" element={<EnterPin />} />
+
+        <Route path="/auth">
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="forgotpassword" element={<ForgotPassword />} />
+          <Route path="pin" element={<EnterPin />} />
+        </Route>
+
+        <Route element={<HomeLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
         <Route element={<DashboardLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/topup" element={<TopUp />} />
@@ -45,9 +51,19 @@ function DashboardLayout() {
     <div className="relative">
       <LoggedNavbar />
       <div className="flex flex-col-reverse md:flex-row justify-between ">
-        <Sidebar cName="w-min" />
+        <Sidebar cName="w-min md:min-h-screen" />
         <Outlet />
+        <div className="md:w-25"></div>
       </div>
+    </div>
+  );
+}
+function HomeLayout() {
+  return (
+    <div className="relative">
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 import { Dashboard, Exit, History, Profile, TopUp, TransferMobile } from '../assets/Svg'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Sidebar({ cName }) {
-    const sz = (window.innerWidth<768)? '30%':'20';
+    const location = useLocation();
+    const sz = (window.innerWidth < 768) ? '30%' : '20';
+
     const items = [
-        { svg: <Dashboard isActive={true} size={sz}/>, text: "Dashboard", isActive: true, link:'/dashboard' },
-        { svg: <TransferMobile size={sz}/>, text: "Transfer", link:'/' },
-        { svg: <History size={sz}/>, text: "History", link:'/' },
-        { svg: <TopUp size={sz}/>, text: "Top Up", link:'/' },
-        { svg: <Profile size={sz} />, text: "Profile", link:'/profile/edit' },
-        { svg: <Exit size={sz} color='#f00'/>, text: "Keluar", link:'/' },
+        { svg: <Dashboard isActive={location.pathname.startsWith('/dashboard')} size={sz} />, text: "Dashboard", link: '/dashboard' },
+        { svg: <TransferMobile isActive={location.pathname.startsWith('/transaction/transfer')} size={sz} />, text: "Transfer", link: 'transaction/transfer' },
+        { svg: <History isActive={location.pathname.startsWith('/transaction/history')} size={sz} />, text: "History", link: 'transaction/history' },
+        { svg: <TopUp isActive={location.pathname.startsWith('/transaction/topup')} size={sz} />, text: "Top Up", link: 'transaction/topup' },
+        { svg: <Profile isActive={location.pathname.startsWith('/profile/edit')} size={sz} />, text: "Profile", link: '/profile/edit' },
+        { svg: <Exit isActive={location.pathname.startsWith('/')} size={sz} color='#f00' />, text: "Keluar", link: '/' },
     ];
 
     return (
         <aside
-            className={`fixed md:static text-[3vw] border-t md:text-[18px] text-[#4F5665] ${cName} p-3 md:ps-8 pt-5 border-r border-[#E8E8E8] z-9999 bg-white md:bg-transparent flex bottom-0 md:block w-screen md:w-min justify-between`}
+            className={`fixed md:static text-[3vw] border-t md:text-[18px] text-[#4F5665] ${cName} p-3 md:ps-[5%] pt-10 border-r border-[#E8E8E8] z-9999 bg-white md:bg-transparent flex bottom-0 md:flex-col w-screen md:w-full md:max-w-xs justify-between md:justify-start md:gap-8`}
         >
-            {items.map((e, i) => {
-
+            {items.map((e) => {
+                const isActive = location.pathname.startsWith(e.link);
                 return (
-                    <Link to={e.link}>
-                        <Item src={e.src} text={e.text} svg={e.svg} isActive={e.isActive} key={i} />
+                    <Link to={e.link} key={e.id}>
+                        <Item
+                            text={e.text}
+                            svg={e.svg}
+                            isActive={e.link != '/' && isActive}
+                        />
                     </Link>
                 )
             })}
@@ -32,12 +38,13 @@ function Sidebar({ cName }) {
 function Item({ text, isActive, svg }) {
     return (
         <div
-            className={`flex flex-col md:flex-row text-[#4F5665] items-center ${isActive ? "text-blue-800 md:bg-[#2948ff] md:text-white" : "hover:border-[#c3c3c3]"} border border-transparent cursor-pointer ${text == "Keluar" && "text-[#D00000] hidden md:flex"} gap-2 md:gap-3 p-2 px-2 rounded-md w-fit md:w-48 `}
+            className={`flex flex-col md:flex-row items-center border border-transparent cursor-pointer gap-2 md:gap-3 p-2 rounded-md w-fit md:w-[90%]
+        ${isActive ? "text-blue-800 md:bg-[#2948ff] md:text-white" : "text-[#4F5665] hover:border-[#c3c3c3]"}
+        ${text === "Keluar" ? "text-[#D00000] hidden md:flex" : ""}`}
         >
             {svg}
-            < p
-            > {text}</p >
-        </div >
+            <p>{text}</p>
+        </div>
     )
 }
 
