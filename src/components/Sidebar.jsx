3660109a2f9ccbede1a89828619cb95cfd07036e
siddiq24 <siddiqrm24@gmail.com>
2025-09-26 10:01:14 +0700

    fix(sidebar) change route link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,10 +7,10 @@ function Sidebar({ cName }) {
     const sz = (window.innerWidth < 768) ? '30%' : '20';
 
     const items = [
-        { svg: <Dashboard isActive={location.pathname.startsWith('/dashboard')} size={sz} />, text: "Dashboard", link: '/dashboard' },
-        { svg: <TransferMobile isActive={location.pathname.startsWith('/transaction/transfer')} size={sz} />, text: "Transfer", link: 'transaction/transfer' },
-        { svg: <History isActive={location.pathname.startsWith('/transaction/history')} size={sz} />, text: "History", link: 'transaction/history' },
-        { svg: <TopUp isActive={location.pathname.startsWith('/transaction/topup')} size={sz} />, text: "Top Up", link: 'transaction/topup' },
+        { svg: <Dashboard isActive={location.pathname.startsWith('/transaction/dashboard')} size={sz} />, text: "Dashboard", link: '/transaction/dashboard' },
+        { svg: <TransferMobile isActive={location.pathname.startsWith('/transaction/transfer')} size={sz} />, text: "Transfer", link: '/transaction/transfer' },
+        { svg: <History isActive={location.pathname.startsWith('/transaction/history')} size={sz} />, text: "History", link: '/transaction/history' },
+        { svg: <TopUp isActive={location.pathname.startsWith('/transaction/topup')} size={sz} />, text: "Top Up", link: '/transaction/topup' },
         { svg: <Profile isActive={location.pathname.startsWith('/profile/edit')} size={sz} />, text: "Profile", link: '/profile/edit' },
         { svg: <Exit isActive={location.pathname.startsWith('/')} size={sz} color='#f00' />, text: "Keluar", link: '/' },
     ];
@@ -19,10 +19,10 @@ function Sidebar({ cName }) {
         <aside
             className={`fixed md:static text-[3vw] border-t md:text-[18px] text-[#4F5665] ${cName} p-3 md:ps-[5%] pt-10 border-r border-[#E8E8E8] z-9999 bg-white md:bg-transparent flex bottom-0 md:flex-col w-screen md:w-full md:max-w-xs justify-between md:justify-start md:gap-8`}
         >
-            {items.map((e) => {
+            {items.map((e, i) => {
                 const isActive = location.pathname.startsWith(e.link);
                 return (
-                    <Link to={e.link} key={e.id}>
+                    <Link to={e.link} key={i}>
                         <Item
                             text={e.text}
                             svg={e.svg}
