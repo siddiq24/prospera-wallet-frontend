3d27e21a43b260c630ee802d8d4bf6f5c51d9cea
malailiyati <malailiyati107@gmail.com>
2025-09-27 14:43:05 +0700

    fixing useNavigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router";
 
 export function Navbar() {
   const [openMenu, setOpenMenu] = useState(false);
-  const navigate = useNavigate();
 
   return (
     <header className="text-white">
